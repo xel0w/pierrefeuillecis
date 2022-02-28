e4101e6c8392f029a53160701d19eb1393ae1f49
xel0w <jordan.byrgiel@hotmail.fr>
2022-02-28 15:19:48 +0100

    maj responsive

--- asset/js/app.js
+++ asset/js/app.js
@@ -8,6 +8,7 @@ const youLoose = document.querySelector(".loose");
 const imgJoueur = document.querySelector(".imgResultJoueur");
 const imgOrdi = document.querySelector(".imgResultOrdi");
 const res = document.querySelector(".res");
+const btnWin = document.querySelectorAll(".replay");
 let compteurJoueur = spanJoueur.innerHTML;
 let compteurOrdi= spanOrdi.innerHTML;
 const choixJoueur = document.querySelectorAll(".bouton");
@@ -33,30 +34,33 @@ function removeClass(){
 function checkIfWin(){
     if(compteurJoueur === 3){
         youWin.classList.add("active")
-        compteurJoueur = 0;
-        compteurOrdi = 0;
+        reponse = true
+        compteurJoueur = 0
+        compteurOrdi = 0
         spanOrdi.innerHTML = compteurOrdi
         spanJoueur.innerHTML = compteurJoueur
-        res.innerHTML="Appuie sur Enter pour rejouer"
-    }
+        res.innerHTML=" "
+        
+    };
     if(compteurOrdi === 3){
         youLoose.classList.add("active")
-        compteurJoueur = 0;
-        compteurOrdi = 0;
+        reponse=true
+        compteurJoueur = 0
+        compteurOrdi = 0
         spanOrdi.innerHTML = compteurOrdi
         spanJoueur.innerHTML = compteurJoueur
-        res.innerHTML="Appuie sur Enter pour rejouer"
-        
-    }
-    document.addEventListener("keyup",(e)=>{
-        if(e.key ==="Enter"){
-        youLoose.classList.remove("active")
-        youWin.classList.remove("active")
-        removeClass()
-        res.innerHTML="La partie peut commencer"
-    }
+        res.innerHTML=" "
+    };
+    btnWin.forEach(function(btn){
+        btn.addEventListener("click",()=>{
+        youLoose.classList.remove("active");
+        youWin.classList.remove("active");
+        removeClass();
+        res.innerHTML="La partie peut commencer<br>Le premier a 3 points gagne";
+        reponse = false;
+        })
     })
-}
+};
 choixJoueur.forEach(function(item){
 item.addEventListener("click",()=>{
     const coup = joueCoupAleatoire();
@@ -66,26 +70,26 @@ item.addEventListener("click",()=>{
         imgJoueur.classList.add("addImagePierre")
         imgOrdi.classList.add("addImagePierre")
         res.innerHTML="Egalité"
-        
+        checkIfWin()
     }
     else if (item.innerHTML == "Feuille" &&  coup == "Feuille") {
         imgJoueur.classList.add("addImageFeuille")
         imgOrdi.classList.add("addImageFeuille")
         res.innerHTML="Egalité"
-        
+        checkIfWin()
     }
     else if (item.innerHTML == "Ciseaux" &&  coup == "Ciseaux") {
         imgJoueur.classList.add("addImageCiseaux")
         imgOrdi.classList.add("addImageCiseaux")
         res.innerHTML="Egalité"
-        
+        checkIfWin()
     }
     else if (item.innerHTML == "Pierre" &&  coup == "Ciseaux") {
         imgJoueur.classList.add("addImagePierre")
         imgOrdi.classList.add("addImageCiseaux")
         compteurJoueur++
         res.innerHTML="Tu marques un point"
-        
+        checkIfWin()
     }
     else if (item.innerHTML == "Feuille" &&  coup == "Pierre") {
         
@@ -93,7 +97,7 @@ item.addEventListener("click",()=>{
         imgOrdi.classList.add("addImagePierre")
         compteurJoueur++
         res.innerHTML="Tu marques un point"
-        
+        checkIfWin()
     }
     else if (item.innerHTML == "Ciseaux" &&  coup == "Feuille") {
         
@@ -101,7 +105,7 @@ item.addEventListener("click",()=>{
         imgOrdi.classList.add("addImageFeuille")
         compteurJoueur++
         res.innerHTML="Tu marques un point"
-        
+        checkIfWin()
     }
     else if (item.innerHTML == "Pierre" &&  coup == "Feuille") {
         
@@ -109,7 +113,7 @@ item.addEventListener("click",()=>{
         imgOrdi.classList.add("addImageFeuille")
         compteurOrdi++
         res.innerHTML="L'ordinateur marques un point"
-        
+        checkIfWin()
         
     }
     else if (item.innerHTML == "Feuille" &&  coup == "Ciseaux") {
@@ -118,7 +122,7 @@ item.addEventListener("click",()=>{
         imgOrdi.classList.add("addImageCiseaux")
         compteurOrdi++
         res.innerHTML="L'ordinateur marques un point"
-        
+        checkIfWin()
     }
     else if (item.innerHTML == "Ciseaux" &&  coup == "Pierre") {
         
@@ -126,12 +130,11 @@ item.addEventListener("click",()=>{
         imgOrdi.classList.add("addImagePierre")
         compteurOrdi++
         res.innerHTML="L'ordinateur marques un point"
-        
+        checkIfWin()
     }
     
     spanJoueur.innerHTML = compteurJoueur;
     spanOrdi.innerHTML = compteurOrdi;
-    checkIfWin()
 
 });
 });
